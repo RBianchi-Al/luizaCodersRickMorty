5030744313a17900c58ee0b272f91ef97d9c67f8
RBianchi-Al <renatalebianchi@gmail.com>
2021-08-22 19:51:16 -0300

    'css'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import api from './service/api'
+
+import LoadImage from './assets/giphy.webp'
+import LogoRick from './assets/logo.png'
 
 function App() {
+  const [rick, setRick] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    api.get("/character").then(
+      res => {
+        setRick(res.data.results)
+        setIsLoading(false)
+      }
+    )
+  }, [])
+  if (isLoading) {
+    return (
+      <div>
+        <img src={LoadImage} alt="loading" />
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <div className="container">
+
+        <div className="logo-content">
+          <img src={LogoRick} alt="Logo"  height="100px" width="auto"/>
+        </div>
+        <div className="cards-content">
+          { rick.map( (item, index) => (
+            <div key={index} className="card">
+              <img src={item.image} alt={item.name} width="180px" height="auto"/>
+              <h4>{item.name}</h4>
+            </div>
+          )) }
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
